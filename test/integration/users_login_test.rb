efc4b8e1da44293c10a7935294aabc88c11a52ff
bogdanproger <bogdanrymar39@gmail.com>
2022-05-01 20:41:06 +0300

    first commit

---
+++ test/integration/users_login_test.rb
@@ -0,0 +1,49 @@
+require 'test_helper'
+
+class UsersLoginTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = users(:michael)
+  end
+
+  test "login with invalid information" do
+    get login_path
+    assert_template 'sessions/new'
+    login_params = {
+      email: "",
+      password: ""
+    }
+
+    post login_path, params: { session: login_params }
+    assert_template 'sessions/new'
+    assert_not flash.empty?
+    get root_path
+    assert flash.empty?
+  end
+
+  test "login with valid information followed by logout" do
+    get login_path
+    login_params = {
+      email: @user.email, 
+      password: 'password'
+    }
+    post login_path, params: { session: login_params }
+    assert is_logged_in?
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
+    
+
+    
+
+    
+  end
+end
+
+
+
+
+
