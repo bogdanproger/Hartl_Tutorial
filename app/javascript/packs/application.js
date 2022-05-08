f3150362091c627c208c993c04577ffa8d91aff4
bogdanproger <bogdanrymar39@gmail.com>
2022-05-08 20:54:26 +0300

    Delete user, pagination

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 require("./rails-ujs.js");
+
 //= require rails-ujs
 Rails.start()
 Turbolinks.start()
